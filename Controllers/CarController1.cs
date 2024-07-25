e3007ad5fc65b9874972ae94513b80673d7765a4
Hillson <hillsonshalom@gmail.com>
2024-07-25 17:02:20 +0300

    Add project files.

---
+++ Controllers/CarController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCQuiz.Models;
+
+namespace MVCQuiz.Controllers
+{
+    public class CarController : Controller
+    {
+
+        public IActionResult Model(string year, string name, string color)
+        {
+            Car car = new Car { name = name ?? "Car", year = year ?? "Unknown", color = color ?? "red" };
+            return View(car);
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCQuiz.Models
+{
+    public class Car
+    {
+        public string year {  get; set; }
+        public string name { get; set; }
+        public string color { get; set; }
+    }
+}
